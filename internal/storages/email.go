3ea2276c9baf35c60b4e2d58c19b977464334d83
Cyclops <vnikita3@gmail.com>
2023-03-02 21:18:45 +0300

    Добавлен функционал для работы с отчетами Email

---
+++ internal/storages/email.go
@@ -0,0 +1,25 @@
+package storages
+
+import (
+	"github.com/CyclopsV/service-status-skillbox/internal/email"
+	"github.com/CyclopsV/service-status-skillbox/pkg/pars"
+)
+
+type EmailStorage []*email.Email
+
+func (es *EmailStorage) Add(obj *email.Email) {
+	*es = append(*es, obj)
+}
+
+func NewEmailStorage(filename string) *EmailStorage {
+	emailStr := pars.ParseFile(filename)
+	es := EmailStorage{}
+	for _, s := range emailStr {
+		res := email.FromSTR(s)
+		if res == nil {
+			continue
+		}
+		es.Add(res)
+	}
+	return &es
+}
